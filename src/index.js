b5fdf2a488e73aa2564bb9af6a0a60462008348d
Kavya Katal <kavyakatal09@gmail.com>
2025-09-04 17:36:10 +0530

    Dynamic Routing

--- src/index.js
+++ src/index.js
@@ -15,6 +15,19 @@ app.get("/about",(req,res)=>{
     res.send("This is About Page")
 })
 
+app.get("/prn/:value",(req,res)=>{
+
+    const products = ['Mango',"Apple","Banana"]
+    const message = "No Product Found"
+
+    if (req.params.value < products.length){
+    res.send(`${products[req.params.value]}`)
+    }else{
+        res.send(message)
+    }
+
+})
+
 
 
 app.listen(3000,()=>{console.log("Server Started on 3000")})
